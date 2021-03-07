0d3218016a92ed971e2761be72c5b9a50535a64a
Nicolas <nicolas@valade.mobi>
2021-03-07 20:19:31 +0100

    systeme de diaporama v1.0

--- maquettes/slides/slides.js
+++ maquettes/slides/slides.js
@@ -2,31 +2,65 @@
 let slideshow = document.getElementById("slideshow");
 let slideshow_images = slideshow.children;
 let images_count = slideshow_images.length;
+let slideControl = document.getElementById("leap");
 let slideIndex = 0;
 var slide;
 
-function autoSlide() {
+for (let i = 0; i < images_count; i++) { // Créer les boutons 1, 2, 3...
+    slideControl.innerHTML += "<div id=\"" + i + "\" onclick=\"goToIndex(" + i + ")\" class=\"slide-button\"></div>"
+}
+let slideButtons = slideControl.children;
+
+function incrementIndex() { // Cibler l'image suivante
+    if (slideIndex == (images_count-1)) {
+        slideIndex = 0;
+    } else {
+        slideIndex++;
+    }
+}
+
+function decrementIndex(){ // Cibler l'image précédente
+    if (slideIndex != 0) { 
+        slideIndex--;
+    } else{
+        slideIndex = (images_count-1)
+    }
+}
+
+function refreshSlide() { // Afficher l'image ciblée
+    for (let i = 0; i < images_count; i++) {
+        slideshow_images[i].style.display = "none";
+        slideButtons[i].classList.remove("active");
+    }
+    slideshow_images[slideIndex].style.display = "block";
+    slideButtons[slideIndex].classList.add("active");
+}
+
+function autoSlide() { // Démarrer le mode automatique
     slide = setInterval(function () {
-        nextSlide();
+        incrementIndex(); // Cibler l'image suivante
+        refreshSlide(); // Afficher l'image ciblée
     }, 3000);
 }
 
-function manualSlide() {
-    nextSlide();
-    clearInterval(slide);
-    autoSlide();
-}
-
 function nextSlide() {
-    for (let i = 0; i < images_count; i++) {
-        slideshow_images[i].style.display = "none";
-    }
-    slideshow_images[slideIndex].style.display = "block";
-    slideIndex++;
-    if (slideIndex == images_count) {
-        slideIndex = 0;
-    }
+    clearInterval(slide); // Arrêter le mode automatique
+    incrementIndex(); // Cibler l'image suivante
+    refreshSlide(); // Afficher l'image ciblée
+    autoSlide(); // Démarrer le mode automatique
 }
 
-nextSlide();
-autoSlide();
+function previousSlide() {
+    clearInterval(slide); // Arrêter le mode automatique
+    decrementIndex() // Cibler l'image précédente
+    refreshSlide(); // Afficher l'image ciblée
+    autoSlide(); // Démarrer le mode automatique
+}
+
+function goToIndex(j) {
+    slideIndex = j; // Cibler l'image j
+    refreshSlide(); // Afficher l'image ciblée
+}
+
+refreshSlide(); // Afficher l'image ciblée
+autoSlide(); // Démarrer le mode automatique
