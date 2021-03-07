c4179c39e86a74d2f94a49598d331fb620e13138
Nicolas <nicolas@valade.mobi>
2021-03-07 20:20:17 +0100

    index.php à tester si les include marchent bien

--- maquettes/index.php
+++ maquettes/index.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="header.css">
-    <link rel="stylesheet" href="slides.css">
     <title>Snack LM</title>
 </head>
 
@@ -15,10 +12,9 @@
 car cible davantage une utilisation smartphone -->
 
 <body>
-    <!-- <?php require 'header.php';?> -->
+    <?php require 'header.php'; ?>
     <h1>Bienvenue</h1>
-    <!-- <?php require 'slides.php';?> -->
-    
+    <?php require 'slides.php'; ?>
 </body>
 
 </html>
