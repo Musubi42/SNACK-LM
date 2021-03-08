2ad6a5fc09e0570e727368a93f6bed4341b454c3
Nicolas <nicolas@valade.mobi>
2021-03-08 01:42:48 +0100

    parlons-en

--- maquettes/index.php
+++ maquettes/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
     <title>Snack LM</title>
 </head>
 
@@ -13,8 +14,22 @@ car cible davantage une utilisation smartphone -->
 
 <body>
     <?php include 'header/header.html'; ?>
-    <h1>Bienvenue</h1>
-    <?php include 'slideshow/slideshow.html'; ?>
+    <div style="text-align:center">
+        <button style="width:80%;margin:8px 8px 4px 8px;height:30px">Appelez-nous</button>
+        <button style="width:80%;margin:4px 8px 8px 8px;height:30px">Commandez en ligne</button>
+    </div>
+
+    <h2>Snack LM, Lorem ipsum...</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat.
+    </p>
+
+    <img id="carte" src="images/Carte.png" alt="Carte">
+    <!-- php include 'slideshow/slideshow.html';  -->
+
 </body>
 
 
